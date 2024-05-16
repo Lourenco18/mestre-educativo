dcaf8a452cfe86170ce99e3eeb4af09cd3216392
Lourenco18 <danielsantos7a@hotmail.com>
2024-05-16 18:26:02 +0100

    nada

--- php/include/forms.inc.php
+++ php/include/forms.inc.php
@@ -62,7 +62,7 @@
         $arrResultados = my_query($consulta);
       
       } else {
-        if (isset($arrResultados[$k]['foto_' . $tabela])) {
+        if (count($arrResultados[$k]['foto_' . $tabela])=0) {
           $foto = '';
           include $arrConfig['dir_admin'] . '/fotos.inc.php';
         }
@@ -83,8 +83,7 @@
           $buttonMsg = "Alterar foto - $tabela";
         }
       } else {
-        $foto = '';
-      
+       $foto = array();
         $buttonMsg = "Inserir foto - $tabela";
       
       }
@@ -262,14 +261,13 @@ if(count($arrRelacao) == 0){
                   <div class="card h-70 ps-0 py-xl-3" style="background-color: white; transition: all 0.3s ease;" onmouseover="this.style.transform=\'scale(1.05)\'; this.style.boxShadow=\'0 4px 8px 0 #696cff, 0 6px 20px 0 #696cff\'; this.style.zIndex=\'1\';" onmouseout="this.style.transform=\'scale(1)\'; this.style.boxShadow=\'none\';">    
                           <div class="card-body" style="text-align: center;height: 231.599258px;  margin-left: 0px">
                               <h5 class="card-title">'.$v['pessoa'].'</h5>
-                              <h6 class="card-title">'.$v['telefone_pessoa'].'</h6>
+                              <h6 class="card-title">'.$v['telefone_pessoa'].'</h6> 
                               <h6 class="card-title">'.$v['relacao'].'</h6>
                             
                           </div>
-                      </div>
+                    </div>
                   </a>
-              </div>
-              ';
+              </div>';
   }
 }
 
